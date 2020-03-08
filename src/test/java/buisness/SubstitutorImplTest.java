ffb9627c544e09d3e3c572f66d37765e95cc37ef
Seweryn91 <s.talaj@gmail.com>
2020-03-08 18:09:41 +0100

    Implement SubstitutorImplTest class

---
+++ src/test/java/buisness/SubstitutorImplTest.java
@@ -0,0 +1,27 @@
+package buisness;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SubstitutorImplTest {
+
+    private SubstitutorImpl substitutor = new SubstitutorImpl();
+
+    @Test
+    void testSubstitute() {
+        String input = "MERRY";
+        String expected = "MARRY";
+        String actual = substitutor.substitute(2, "A", input);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void testSubstitute_longWord() {
+        String input = "THISISVERYLONGWORDIDONOTEVENKNOWWHYIWROTETHATTHISWAY";
+        String expected = "THISISVERYLONGWORDIDONOTEVENKNOWWHYUWROTETHATTHISWAY";
+        String actual= substitutor.substitute(36, "U", input);
+        assertEquals(expected, actual);
+    }
+
+}
