6e6900ebdde60c4f7e259ba3a380bcefb1c66def
Seweryn91 <s.talaj@gmail.com>
2020-03-08 17:15:51 +0100

    Implement TextFileWriter class

---
+++ src/main/java/buisness/TextFileWriter.java
@@ -0,0 +1,25 @@
+package buisness;
+
+
+import java.io.*;
+
+public class TextFileWriter {
+
+    public void writeToFile(String path, String text, boolean isDecoded) throws IOException {
+        String newPath = path.substring(0, path.lastIndexOf('/'));
+        String filename = "Encoded";
+        if (isDecoded) {
+            filename = "Decoded";
+        }
+        BufferedWriter output = null;
+        try {
+            File file = new File(newPath + "/" + filename + ".txt");
+            output = new BufferedWriter(new FileWriter(file));
+            output.write(text);
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            output.close();
+        }
+    }
+}
